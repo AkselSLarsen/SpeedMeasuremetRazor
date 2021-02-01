73a2f7cabdbbd710e0c96cb9f6cf9bf3506511c0
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2021-02-01 12:54:05 +0100

    LocationRepo metoder og constructor

--- SpeedMeasuremetRazor/Services/LocationRepo.cs
+++ SpeedMeasuremetRazor/Services/LocationRepo.cs
@@ -11,8 +11,52 @@ namespace SpeedMeasuremetRazor.Services
 {
     
 
-    public class LocationRepo 
+    public class LocationRepo : ILocationRepo
     {
-       
+        private List<Location> _locations;
+
+        
+
+        public LocationRepo()
+        {
+            _locations= MockData.Locations;
+        }
+
+        public List<Location> GetAllLocations()
+        {
+            return _locations;
+        }
+
+        public void AddLocation(Location location)
+        {
+            _locations.Add(location);
+        }
+
+        public void UpdateLocation(Location location)
+        {
+            Location toBeChanged = GetLocation(location.Id);
+            toBeChanged.Address = location.Address;
+            toBeChanged.SpeedLimit = location.SpeedLimit;
+            toBeChanged.Zone = location.Zone;
+        }
+
+        public Location GetLocation(int id)
+        {
+            Location result = null;
+            foreach (var location in _locations)
+            {
+                if (location.Id == id)
+                {
+                    result = location;
+                }
+            }
+
+            return result;
+        }
+
+        public void DeleteLocation(int id)
+        {
+            _locations.Remove(GetLocation(id));
+        }
     }
 }
