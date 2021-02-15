bf2032f2a44d5a548e59d13864c3e1efeaf880ce
Sebastian Løgtholt Westergaard <seba255j@edu.easj.dk>
2021-02-15 12:50:30 +0100

    Sortering på locations

--- SpeedMeasuremetRazor/Pages/Locations/Index.cshtml.cs
+++ SpeedMeasuremetRazor/Pages/Locations/Index.cshtml.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SpeedMeasuremetRazor.Helpers;
 using SpeedMeasuremetRazor.Interfaces;
+using SpeedMeasuremetRazor.Models;
 using SpeedMeasuremetRazor.Services;
 
 namespace SpeedMeasuremetRazor.Pages.Locations
@@ -12,7 +14,7 @@ namespace SpeedMeasuremetRazor.Pages.Locations
     public class IndexModel : PageModel
     {
         private ILocationRepo _locationRepo;
-
+        public List<Location> _locationSortedList;
         public ILocationRepo LocationRepo {
             get { return _locationRepo; }
             set { _locationRepo = value; }
@@ -20,12 +22,31 @@ namespace SpeedMeasuremetRazor.Pages.Locations
 
         public IndexModel(ILocationRepo locationRepo) {
             _locationRepo = (ILocationRepo)locationRepo;
+            _locationSortedList = _locationRepo.GetAllLocations();
         }
 
         public void OnGet()
         {
         }
 
+        public IActionResult OnPostSort(int sort)
+        {
+            if (sort == 1)
+            {
+                _locationSortedList.Sort();
+            }
+            if (sort == 2)
+            {
+                _locationSortedList.Sort(new LocationSortBySpeed());
+            }
+
+            if (sort == 3)
+            {
+                _locationSortedList.Sort(new LocationSortByZone());
+            }
+
+            return Page();
+        }
 
     }
 }
